0d35361c8ea6d9b318669db3537ecffe6f8f3870
Safi <ensahinsafi@gmail.com>
2025-06-24 22:33:08 +0600

    feat(Navigation): add navigation transition animation.

--- app/src/main/java/com/example/navigation3/navigation/NavRoute.kt
+++ app/src/main/java/com/example/navigation3/navigation/NavRoute.kt
@@ -1,6 +1,11 @@
 package com.example.navigation3.navigation
 
-sealed class NavRoute {
+import androidx.navigation3.runtime.NavKey
+import kotlinx.serialization.Serializable
+
+sealed class NavRoute : NavKey {
+    @Serializable
     data object NoteScreenRoute : NavRoute()
+    @Serializable
     data class NoteDetailsScreenRoute(val id : String) : NavRoute()
 }
--- app/src/main/java/com/example/navigation3/navigation/Navigation.kt
+++ app/src/main/java/com/example/navigation3/navigation/Navigation.kt
@@ -1,9 +1,12 @@
 package com.example.navigation3.navigation
 
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
+import androidx.compose.animation.togetherWith
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.remember
-import androidx.navigation3.runtime.NavEntry
+import androidx.navigation3.runtime.entry
+import androidx.navigation3.runtime.entryProvider
+import androidx.navigation3.runtime.rememberNavBackStack
 import androidx.navigation3.ui.NavDisplay
 import com.example.navigation3.screens.NoteDetailsScreen
 import com.example.navigation3.screens.NoteScreen
@@ -11,24 +14,54 @@ import com.example.navigation3.screens.NoteScreen
 @Composable
 fun Navigation() {
 
-    val backStack = remember { mutableStateListOf<NavRoute>(NavRoute.NoteScreenRoute) }
+    // val backStack = remember { mutableStateListOf<NavRoute>(NavRoute.NoteScreenRoute) }
+    val backStack = rememberNavBackStack<NavRoute>(NavRoute.NoteScreenRoute) // survive configure change
 
     NavDisplay(
         backStack = backStack,
         onBack = { backStack.removeLastOrNull() },
-        entryProvider = { key ->
-            when(key) {
-                is NavRoute.NoteScreenRoute -> NavEntry(key) {
-                    NoteScreen(
-                        onClickDetailsBtn = {
-                            backStack.add(NavRoute.NoteDetailsScreenRoute(id = key.toString()))
-                        }
-                    )
-                }
-                is NavRoute.NoteDetailsScreenRoute -> NavEntry(key) {
-                    NoteDetailsScreen(key.id)
-                }
+//        entryProvider = { key ->
+//            when(key) {
+//                is NavRoute.NoteScreenRoute -> NavEntry(key) {
+//                    NoteScreen(
+//                        onClickDetailsBtn = {
+//                            backStack.add(NavRoute.NoteDetailsScreenRoute(id = key.toString()))
+//                        }
+//                    )
+//                }
+//                is NavRoute.NoteDetailsScreenRoute -> NavEntry(key) {
+//                    NoteDetailsScreen(key.id)
+//                }
+//            }
+//        }
+        entryProvider = entryProvider {
+
+            entry<NavRoute.NoteScreenRoute> { key ->
+                NoteScreen(
+                    onClickDetailsBtn = {
+                        backStack.add(NavRoute.NoteDetailsScreenRoute(id = key.toString()))
+                    }
+                )
             }
+
+            entry<NavRoute.NoteDetailsScreenRoute> { key ->
+                NoteDetailsScreen(key.id)
+            }
+        },
+        transitionSpec = {
+            // Slide in from right when navigating forward
+            slideInHorizontally(initialOffsetX = { it }) togetherWith
+                    slideOutHorizontally(targetOffsetX = { -it })
+        },
+        popTransitionSpec = {
+            // Slide in from left when navigating back
+            slideInHorizontally(initialOffsetX = { -it }) togetherWith
+                    slideOutHorizontally(targetOffsetX = { it })
+        },
+        predictivePopTransitionSpec = {
+            // Slide in from left when navigating back
+            slideInHorizontally(initialOffsetX = { -it }) togetherWith
+                    slideOutHorizontally(targetOffsetX = { it })
         }
     )
 }
